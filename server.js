20f4468bd82a7a62dd379f0cc9e0dcd5cb5f6b6c
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-11-10 17:32:13 +0000

    changes for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 const app = express();
@@ -19,6 +20,14 @@ mongoose
 
 // use routes
 app.use("/api/items", items);
+
+// server static assets (build folder) if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // starting server
 const PORT = process.env.PORT || 5000;
